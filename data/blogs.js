6f7a2ed3afd8cfc31e87c047bd6b4a1f14573db0
SuyashDhiman856 <suyashdhiman856@gmail.com>
2024-08-25 20:27:54 +0530

    seventh commit

--- data/blogs.js
+++ data/blogs.js
@@ -41,7 +41,113 @@ blogs = {
             slug: "learn-angular",
             image: "../images/angular.png",
             remark: "new"
-        }
+        },
+        {
+            title: "Learn C++",
+            content: "Get started with C++",
+            slug: "learn-cpp",
+            image: "../images/cpp.png",
+            remark: "new"
+        },
+        {
+            title: "Learn CSS",
+            content: "Get started with CSS",
+            slug: "learn-css",
+            image: "../images/css.jpg",
+            remark: "popular"
+        },
+        {
+            title: "Learn Django",
+            content: "Get started with Django",
+            slug: "learn-django",
+            image: "../images/django.png",
+            remark: "new"
+        },
+        {
+            title: "Learn Express JS",
+            content: "Get started with Express JS",
+            slug: "learn-express",
+            image: "../images/expressjs.png",
+            remark: "popular"
+        },
+        {
+            title: "Learn Flask",
+            content: "Get started with Flask",
+            slug: "learn-flask",
+            image: "../images/flask.png",
+            remark: "new"
+        },
+        {
+            title: "Learn GIT",
+            content: "Get started with GIT",
+            slug: "learn-git",
+            image: "../images/git.png",
+            remark: "popular"
+        },
+        {
+            title: "Learn GO LANG",
+            content: "Get started with GO LANG",
+            slug: "learn-golang",
+            image: "../images/go.png",
+            remark: "new"
+        },
+        {
+            title: "Learn HTML",
+            content: "Get started with HTML",
+            slug: "learn-html",
+            image: "../images/html.jpg",
+            remark: "popular"
+        },
+        {
+            title: "Learn Mongo DB",
+            content: "Get started with Mongo DB",
+            slug: "learn-mongodb",
+            image: "../images/mongodb.jpg",
+            remark: "new"
+        },
+        {
+            title: "Learn MySQL",
+            content: "Get started with MySQL",
+            slug: "learn-mysql",
+            image: "../images/mysql.jpg",
+            remark: "popular"
+        },
+        {
+            title: "Learn Node JS",
+            content: "Get started with Node JS",
+            slug: "learn-nodejs",
+            image: "../images/nodejs.png",
+            remark: "new"
+        },
+        {
+            title: "Learn PHP",
+            content: "Get started with PHP",
+            slug: "learn-php",
+            image: "../images/php.jpg",
+            remark: "popular"
+        },
+        {
+            title: "Learn React Native",
+            content: "Get started with React Native",
+            slug: "learn-react-native",
+            image: "../images/reactNative.png",
+            remark: "new"
+        },
+        {
+            title: "Learn Rust",
+            content: "Get started with Rust",
+            slug: "learn-rust",
+            image: "../images/rust.jpg",
+            remark: "popular"
+        },
+        {
+            title: "Learn Vue JS",
+            content: "Get started with Vue JS",
+            slug: "learn-vue",
+            image: "../images/vue.jpg",
+            remark: "new"
+        },
+        
     ]
 }
 
