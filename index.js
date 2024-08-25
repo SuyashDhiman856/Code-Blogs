e61033671a19e8d5482136640d05bcc81af3b6eb
SuyashDhiman856 <suyashdhiman856@gmail.com>
2024-08-25 14:32:46 +0530

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "views")));
 app.use("/images", express.static(path.join(__dirname, "/static/images")));
 app.use('/', require(path.join(__dirname, "../router/router.js")));
 
