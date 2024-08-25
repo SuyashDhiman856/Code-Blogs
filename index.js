186fdf55662253f5131e5c81e13b5ca6e11a7020
SuyashDhiman856 <suyashdhiman856@gmail.com>
2024-08-25 14:30:58 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require('path');
+var exphbs  = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "static")));
+app.use("/images", express.static(path.join(__dirname, "/static/images")));
+app.use('/', require(path.join(__dirname, "../router/router.js")));
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.listen(port, () => {
+    console.log(`Blog App running on http://localhost:${port}`);
+})
